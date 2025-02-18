6d70b910a8374bb7b9a00b24af76471f26d04dec
Wamba Elisha <elishawamba61@gmail.comgit>
2025-02-18 14:53:20 +0100

    react-router implemented

---
+++ eventaide/src/pages/Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div className="page-container">
+      <h1>Our Services</h1>
+      <p>Discover the range of services we offer.</p>
+    </div>
+  );
+};
+
+export default Services;
